f15522ebc218aa3bdd198340a2feab668713d3a8
jolonf <jolonf@5e395760-6f35-0410-9b03-bddf9ffd9815>
2008-03-29 06:49:56 +0000

    Removed Croatian and made changes to shell scripts to support extra level of language directories.

--- trunk/Collections/MakeAll.sh
+++ trunk/Collections/MakeAll.sh
@@ -1,10 +1,18 @@
 #!
 
-foreach file ( * )
-	# if file is a directory and it contains a Collections.txt
-	# file then process it
-	if (-d "$file" && -e "$file/$file.txt") then
-		./MakeCollection.sh "$file"
+foreach language (*)
+	# if file is a directory then it is a language directory that contains translations
+	if (-d "$language") then
+		cd "$language"
+		foreach translation (*)
+			# if file is a directory and it contains a Collections.txt
+			# file then process it
+			if (-d "$translation" && -e "$translation/$translation.txt") then
+				echo $translation
+				../MakeCollection.sh "$translation"
+			endif
+		end
+		cd ..
 	endif
 end
 
